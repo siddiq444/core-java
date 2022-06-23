9ea7c7b76cc0733de528d053f12014b2254786c8
syeds <syeds@DESKTOP-5FT63LT>
2022-06-23 10:26:50 +0100

    even and odd

---
+++ leap year/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int year=2000;       // leap year
+		if(year%4==0)
+			System.out.println("Leap year");
+		else
+			System.out.println("Not Leap year");
+	}
+
+}
